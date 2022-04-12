c9fabf90c9d5782a1c4d65df74a875f06425f20f
clay000095 <85270632+clay000095@users.noreply.github.com>
2022-04-12 17:02:24 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
-
-
-
-
-
-
-
-
-
-
-
-
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun  2 21:16:35 2021
+@author: Ivan
+版權屬於「行銷搬進大程式」所有，若有疑問，可聯絡ivanyang0606@gmail.com
+Line Bot聊天機器人
+第三章 互動回傳功能
+推播push_message與回覆reply_message
+"""
+#載入LineBot所需要的套件
 from flask import Flask, request, abort
 
 from linebot import (
@@ -19,7 +17,7 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import *
-
+import re
 app = Flask(__name__)
 
 # 必須放上自己的Channel Access Token
@@ -56,7 +54,6 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token,TextSendMessage('才不告訴你哩！'))
     else:
         line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
-
 #主程式
 import os
 if __name__ == "__main__":
